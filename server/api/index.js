5174288e3c2023bd5a1bcc1eddea2c65c8b9c5c3
Jason Jacobs <nerfnerd@gmail.com>
2021-10-07 13:59:22 -0700

    Add more messaging

--- server/api/index.js
+++ server/api/index.js
@@ -8,12 +8,15 @@ var Mixpanel = require('mixpanel');
 var mixpanel = Mixpanel.init('d6a6208c71b46a6965913df792f505f9');
 
 
+mixpanel.track('Sommelier:Notification:Subscribe', { end_point: 'create' });
 
 router.get('/send', async (req, res) => {
 
   const data = await Subscription.find().limit(5);
 
   res.status(201).json({ code: 201, version: '0.0.3', data });
+
+  mixpanel.track('Sommelier:Notification:Send', { data: data });
 });
 
 router.post('/subscribe', async (req, res) => {
