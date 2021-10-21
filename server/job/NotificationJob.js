9222749deb0751c07776aa26b4d67a3ec2c5e1a4
wdev733 <wdev733@gmail.com>
2021-10-21 09:31:03 -0700

    fix: send notifications for all upcoming events

--- server/job/NotificationJob.js
+++ server/job/NotificationJob.js
@@ -32,7 +32,7 @@ NotificationJob.prototype.sendNotification = async function() {
   const today = `${d.getFullYear()}-${leadZero(d.getMonth() + 1)}-${leadZero(d.getDate())}`;
   const tomorrow = `${dt.getFullYear()}-${leadZero(dt.getMonth() + 1)}-${leadZero(dt.getDate())}`;
 
-  console.log(hour, min);
+  // console.log(hour, min);
   if ((hour === beforeDayHour || hour === todayHour) && (min === 0)) {
     const response = await axios.get(
       `https://api.storyblok.com/v1/cdn/stories/?starts_with=events&token=${storyblokAccessToken}`
@@ -46,56 +46,60 @@ NotificationJob.prototype.sendNotification = async function() {
       return;
     }
 
-    const story = response.data.stories[0];
-    const startTime = story.content.start_date;
-    const startDate = startTime.split(' ')[0];
-
-    console.log(startDate);
-
-    let shouldSend = false;
-    if ((startDate === today) && (hour === todayHour)) {
-      shouldSend = true;
-    }
-
-    if ((startDate === tomorrow) && (hour === beforeDayHour)) {
-      shouldSend = true;
-    }
-
-    if (shouldSend === false) {
-      return;
-    }
-
-    // console.log(`https://api.storyblok.com/v1/cdn/stories/?token=${storyblokAccessToken}&starts_with=events`);
-    // console.log(story);
-
-    const event = {
-      title: story.content.title,
-      description: `A reminder that ${story.content.title} will be on ${story.content.start_date}`,
-      url: story.content.location,
-      image: story.content.event_image.filename
-    }
-        
-    const payload = JSON.stringify(event);
-    
     const subscriptions = await Subscription.find({});
 
     if (subscriptions.length === 0) {
       return;
     }
 
-    for (let i = 0; i < subscriptions.length; i++) {
-      const subscription = {
-        endpoint: subscriptions[i].endpoint,
-        expirationTime: null,
-        keys: {
-          p256dh: subscriptions[i].p256dh,
-          auth: subscriptions[i].auth
-        }
+    for (let j = 0; j < response.data.stories.length; j++) {
+
+      const story = response.data.stories[j];
+
+      const startTime = story.content.start_date;
+      const startDate = startTime.split(' ')[0];
+
+      // console.log(startDate);
+
+      let shouldSend = false;
+      if ((startDate === today) && (hour === todayHour)) {
+        shouldSend = true;
       }
 
-      webpush.sendNotification(subscription, payload).catch(error => {
-        console.error(error.stack);
-      });
+      if ((startDate === tomorrow) && (hour === beforeDayHour)) {
+        shouldSend = true;
+      }
+
+      if (shouldSend === false) {
+        return;
+      }
+
+      // console.log(`https://api.storyblok.com/v1/cdn/stories/?token=${storyblokAccessToken}&starts_with=events`);
+      // console.log(story);
+
+      const event = {
+        title: story.content.title,
+        description: `A reminder that ${story.content.title} will be on ${story.content.start_date}`,
+        url: story.full_slug,
+        image: story.content.event_image.filename
+      }
+          
+      const payload = JSON.stringify(event);
+
+      for (let i = 0; i < subscriptions.length; i++) {
+        const subscription = {
+          endpoint: subscriptions[i].endpoint,
+          expirationTime: null,
+          keys: {
+            p256dh: subscriptions[i].p256dh,
+            auth: subscriptions[i].auth
+          }
+        }
+
+        webpush.sendNotification(subscription, payload).catch(error => {
+          console.error(error.stack);
+        });
+      }
     }
   }
 }
